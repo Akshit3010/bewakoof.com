98c700abd0a973f88f655a21c4f11def4a386e26
yash-2507 <99003521+yash-2507@users.noreply.github.com>
2022-07-20 18:06:42 +0530

    UI Done

---
+++ frontend/src/pages/SingleProductPage/SingleProductPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/HeaderTop/Header";
+import Navbar from "../../components/Navbar/Navbar";
+import SingleProduct from "../../components/SingleProduct/SingleProduct";
+
+function SingleProductPage() {
+    return (
+        <>
+            <Header />
+            <Navbar />
+            <SingleProduct />
+        </>
+    );
+}
+
+export default SingleProductPage;
--- frontend/src/routes/mainRoute.js
+++ frontend/src/routes/mainRoute.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Wishlist from "../components/WishlistPage/Wishlist";
-import SingleProduct from "../components/SingleProductPage/SingleProduct";
 import { Routes, Route } from "react-router-dom";
+import WishlistPage from "../pages/WishlistPage/WishlistPage";
+import SingleProductPage from "../pages/SingleProductPage/SingleProductPage";
 
 export default function MainRoute() {
     return (
         <>
             <Routes>
-                <Route path="/wishlist" element={<Wishlist />} />
-                <Route path="/singleProduct" element={<SingleProduct />} />
+                <Route path="/wishlist" element={<WishlistPage />} />
+                <Route path="/singleProduct" element={<SingleProductPage />} />
             </Routes>
         </>
     );
