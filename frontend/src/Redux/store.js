8bd602da3bbf0a7d70ba80b02ce141db4e3ceaea
Akshit Rana <akshitrana21@gmail.com>
2022-07-21 23:51:30 +0530

    Products page main section

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,23 +1,25 @@
-import { legacy_createStore as createStore, applyMiddleware, compose,combineReducers, } from 'redux';
-import thunk from 'redux-thunk'
-
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+  combineReducers,
+} from "redux";
+import thunk from "redux-thunk";
+import { reducer } from "./reducer";
 
 const rootReducer = combineReducers({
-   reducer:reducer
+  reducer: reducer,
+});
 
-
-})
-
-
-  const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
 const enhancer = composeEnhancers(
-  applyMiddleware(thunk),
+  applyMiddleware(thunk)
   // other store enhancers if any
 );
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'redux'
-import {store}  from './Redux/store.js'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
-<Provider store={store}>
-<BrowserRouter>
-<App />
-</BrowserRouter>
-</Provider>
-
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
