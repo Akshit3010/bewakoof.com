9db23490b94c5876ff1304b9bd3b21d6e399b21e
Nirbhay Pratap Singh <nirbhaydalnia@gmail.com>
2022-07-22 03:43:57 +0530

    ok

--- server/README.md
+++
@@ -1,3 +0,0 @@
-<h1>Project-Code:heady-rabbits-8957<h1>
-    <h1>Project-Name:Bewakoof.com<h1>
-
--- server/src/Configs/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const db = 'mongodb://localhost:27017/bewakoof'
-const connect =()=>{
- return  mongoose.connect(db)
-}
-module.exports = {connect,db}
