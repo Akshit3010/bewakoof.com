71c6428ec9605bae0d5fcc5170286f32fe46280e
Akshit Rana <akshitrana21@gmail.com>
2022-07-22 17:50:12 +0530

    Connected Database with cloud

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Products from "./pages/Products";
+import MainRoute from "./Routes/MainRoute";
 
 function App() {
   return (
     <div className="App">
-      <Products />
+      <MainRoute />
     </div>
   );
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,13 @@
+const express = require("express");
+const cors = require("cors");
 
-const express = require('express')
-const cors = require('cors');
-const userRouter = require('./routes/user.route');
+const userRouter = require("./routes/user.route");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use('/users',userRouter)
+app.use("/users", userRouter);
 
-
-
-
-module.exports= app
+module.exports = app;
