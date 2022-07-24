64e8823ec6204581be0b8db6635a0e3b7ebd26b3
Akshit Rana <akshitrana21@gmail.com>
2022-07-24 23:43:01 +0530

    final

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
-import { getUserbag } from "../../Redux/action";
 import Menprod from "./Menprod";
 
 const Home = () => {
-  const { user } = useSelector((state) => state.reducer);
-  const dispatch = useDispatch();
   const location = useLocation();
   const query = location?.search?.substring(1);
   useEffect(() => {
     if (query) {
       localStorage.setItem("jwtoken", query);
     }
-    dispatch(getUserbag(user.user._id));
   }, [query]);
   return (
     <>
