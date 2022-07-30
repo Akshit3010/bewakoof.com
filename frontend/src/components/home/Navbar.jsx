f85141cdd5c433a021201e2f2a339f4d914ff397
Akshit Rana <akshitrana21@gmail.com>
2022-07-30 18:43:55 +0530

    Update Navbar.jsx

--- frontend/src/components/home/Navbar.jsx
+++ frontend/src/components/home/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   useEffect(() => {
     dispatch(getUserbag(user?.user?._id));
   }, [isLoggedIn, token, user]);
-
+  console.log(mybag);
   const navigate = useNavigate();
 
   const logout = () => {
