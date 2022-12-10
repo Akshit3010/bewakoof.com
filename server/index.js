0ca606ad46624f9d6e511fd0934e64cbcae6523b
Akshit Rana <akshitrana21@gmail.com>
2022-12-10 13:29:28 +0530

    fixed cors issue

--- server/index.js
+++ server/index.js
@@ -2,14 +2,6 @@ const app = require("./src/index");
 const { connect, db } = require("./src/Configs/db");
 const PORT = process.env.PORT || 8000;
 
-const cors = require("cors");
-
-const corsOptions = {
-  origin: true, //included origin as true
-  credentials: true, //included credentials as true
-};
-app.use(cors(corsOptions));
-
 app.listen(PORT, async () => {
   try {
     await connect();
